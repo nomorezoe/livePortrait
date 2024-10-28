53d6def0aa194a8d4eae28330c71952ef6418ee2
nomorezoe <zoewangwork@gmail.com>
2024-10-28 14:57:16 +0800

    up

--- nodes.py
+++ nodes.py
@@ -461,12 +461,12 @@ class LP_Engine:
 
         if 0 < eyebrow:
             x_d_new[0, 1, 1] += eyebrow * 0.001
-            x_d_new[0, 2, 1] += eyebrow * -0.001
+           # x_d_new[0, 2, 1] += eyebrow * -0.001
         else:
             x_d_new[0, 1, 0] += eyebrow * -0.001
-            x_d_new[0, 2, 0] += eyebrow * 0.001
+           # x_d_new[0, 2, 0] += eyebrow * 0.001
             x_d_new[0, 1, 1] += eyebrow * 0.0003
-            x_d_new[0, 2, 1] += eyebrow * -0.0003
+            #x_d_new[0, 2, 1] += eyebrow * -0.0003
 
 
         return torch.Tensor([rotate_pitch, rotate_yaw, rotate_roll])
