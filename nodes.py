9daf08f340cb6124f593810f66b1a0718687156b
nomorezoe <zoewangwork@gmail.com>
2024-10-28 16:04:37 +0800

    e

--- nodes.py
+++ nodes.py
@@ -464,7 +464,7 @@ class LP_Engine:
             #x_d_new[0, 2, 1] += eyebrow * -0.001
         else:
             x_d_new[0, 1, 0] += eyebrow * -0.001
-            #x_d_new[0, 2, 0] += eyebrow * 0.001
+            x_d_new[0, 2, 0] += eyebrow * 0.001
             x_d_new[0, 1, 1] += eyebrow * 0.0003
             #x_d_new[0, 2, 1] += eyebrow * -0.0003
 
